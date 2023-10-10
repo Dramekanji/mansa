541419575c4706a681dcc0b911204f28aecf3ec3
dramekanji <dramekanjii@gmail.com>
2023-10-10 15:46:16 -0400

    new image for slider

--- components/SliderData.js
+++ components/SliderData.js
@@ -2,10 +2,12 @@ import image2 from "../public/images/pt-still-2.png";
 import image3 from "../public/images/pt-still-3.png";
 import image4 from "../public/images/pt-still-4.png";
 import image5 from "../public/images/pt-still-5.png";
+import image6 from "../public/images/ghs2-still-1.png";
 
 export const SliderData = [
   { image: image2 },
   { image: image3 },
   { image: image4 },
   { image: image5 },
+  { image: image6 },
 ];
