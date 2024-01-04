89f0987b66c04f23172004e85ceadddb825ac969
dramekanji <dramekanjii@gmail.com>
2024-01-03 21:47:52 -0500

    adjusted hero section text mt-20

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = ({ heading, message }) => {
       {/* Overlay*/}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[2]" />
       <div className="p-5 text-white z-[2]  mt-[-10rem]">
-        <h2 className="text-5xl font-bold text-center">{heading}</h2>
+        <h2 className="text-5xl font-bold text-center mt-20">{heading}</h2>
         <p className="py-5 text-xl font-semibold">{message}</p>
         {/* <div className="text-center">
           {" "}
