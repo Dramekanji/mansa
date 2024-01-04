2ad7ccb5b663970ecae04419485a4f9d20afff6d
dramekanji <dramekanjii@gmail.com>
2024-01-03 21:58:54 -0500

    center Hero section next

--- components/Hero.js
+++ components/Hero.js
@@ -3,20 +3,11 @@ import React from "react";
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
-      {/* Overlay*/}
+    <div className="flex flex-col items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60 z-[2]" />
-      <div className="p-5 text-white z-[2]  mt-[-10rem]">
-        <h2 className="text-5xl font-bold text-center mt-20">{heading}</h2>
+      <div className="p-5 text-white z-[2] text-center">
+        <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl font-semibold">{message}</p>
-        {/* <div className="text-center">
-          {" "}
-          <Link href="/Contact">
-            <button className="px-8 py-2 border font-semibold text-white hover:bg-yellow-500 hover:text-black ease-in duration-300 ">
-              Hire Us
-            </button>
-          </Link>
-        </div> */}
       </div>
     </div>
   );
